17aca843124c4c9fbbdf53ecc3cb280d2a02dd0c
jonecastro <143736466+jonecastro@users.noreply.github.com>
2023-09-29 21:30:57 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -11,7 +11,7 @@ def get_big_mac_price_by_year(year, country_code):
     mean_dollar_price = round(df_by_date['dollar_price'].mean(), 2)
     return mean_dollar_price
 
-def get_big_mac_by_country(country_code):
+def get_big_mac_price_by_country(country_code):
     country_code = country_code.upper()
     df_by_country = df[df['iso_a3'] == country_code]
     return round(df_by_country['dollar_price'].mean(), 2)
@@ -21,7 +21,7 @@ def get_the_cheapest_big_mac_price_by_year(year):
     df_by_year = df.query(new_query)
     index_of_min_value = df_by_year['dollar_price'].idxmin()
     cheapest_burger = df_by_year.loc[index_of_min_value]
-    result = f"{cheapest_burger['name']}({cheapest_burger['iso_a3']}), ${round(cheapest_burger['dollar_price'], 2)}"
+    result = f"{cheapest_burger['name']}({cheapest_burger['iso_a3']}): ${round(cheapest_burger['dollar_price'], 2)}"
     return result
 
 
@@ -30,7 +30,7 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     df_by_year = df.query(new_query)
     index_of_max_value = df_by_year['dollar_price'].idxmax()
     expensive_burger = df_by_year.loc[index_of_max_value]
-    result = f"{expensive_burger['name']}({expensive_burger['iso_a3']}), ${round(expensive_burger['dollar_price'], 2)}"
+    result = f"{expensive_burger['name']}({expensive_burger['iso_a3']}): ${round(expensive_burger['dollar_price'], 2)}"
     return result
 
 if __name__ == "__main__":
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     country_code = 'mex'
 
     print("Average Big Mac price by year and country:", get_big_mac_price_by_year(year, country_code))
-    print("Average Big Mac price by country:", get_big_mac_by_country(country_code))
+    print("Average Big Mac price by country:", get_big_mac_price_by_country(country_code))
     print("Cheapest Big Mac price by year:", get_the_cheapest_big_mac_price_by_year(year))
     print("Most Expensive Big Mac price by year:", get_the_most_expensive_big_mac_price_by_year(year))
     
