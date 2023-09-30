fa3448c37baa537c95656624e604860c490e49fb
jonecastro <143736466+jonecastro@users.noreply.github.com>
2023-09-29 19:01:03 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -1,7 +1,8 @@
 import csv
 import pandas as pd
+pd.read_csv("big-mac-full-index.csv")
 
-big_mac_file = './big-mac-full-index.csv'
+big_mac_file = 'big-mac-full-index.csv'
 df = pd.read_csv(big_mac_file)
 
 
@@ -35,7 +36,7 @@ def get_the_most_expensive_big_mac_price_by_year(year):
 
 if __name__ == "__main__":
     year = '2000'
-    country_code = 'mxn'
+    country_code = 'mex'
 
     print("Average Big Mac price by year and country:", get_big_mac_price_by_year(year, country_code))
     print("Average Big Mac price by country:", get_big_mac_by_country(country_code))
