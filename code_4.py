da67593b8441cb2ab2a195381fadca35293f1cf7
jonecastro <143736466+jonecastro@users.noreply.github.com>
2023-09-29 21:42:28 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -7,7 +7,7 @@ print(df.columns)
 
 def get_big_mac_price_by_year(year, country_code):
     country_code = country_code.upper()
-    df_by_date = df[df['date'].str.startswith(str(year)) & (df["iso_a3"] == country_code)]
+    df_by_date = df[df['date'].str.startswith(str(year)) & (df['iso_a3'] == country_code)]
     mean_dollar_price = round(df_by_date['dollar_price'].mean(), 2)
     return mean_dollar_price
 
